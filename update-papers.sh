4f68682f151410f4aed8d8dd8a8f5651f4272104
Eric Medvet <eric.medvet@gmail.com>
2020-12-30 12:51:29 +0100

    full rebuild

--- update-papers.sh
+++ update-papers.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+rm -r content/publications/*
 node update-papers.js 'https://spreadsheets.google.com/feeds/list/1V4DHMqSTC0_ACR7T0FvwdPoxX2X92YMdj2fNqrwnuHg/1/public/values?alt=json' content/publications/ 'Medvet, Eric' 'E'
