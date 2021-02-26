4ec9a68196a2cac643cec8a5ab288a308c8d87e8
Eric Medvet <eric.medvet@gmail.com>
2021-02-26 23:24:39 +0100

    full rebuild

--- update-slides.sh
+++ update-slides.sh
@@ -7,5 +7,9 @@ rm -r static/teaching/1920-advancedprogramming/slides/*
 unzip master.zip "slides-master/advanced-programming-2020/*"
 mv slides-master/advanced-programming-2020/* static/teaching/1920-advancedprogramming/slides
 
+rm -r static/teaching/2021-advancedprogramming/slides/*
+unzip master.zip "slides-master/advanced-programming-2021/*"
+mv slides-master/advanced-programming-2021/* static/teaching/2021-advancedprogramming/slides
+
 rm -r slides-master
 rm -f master.zip
