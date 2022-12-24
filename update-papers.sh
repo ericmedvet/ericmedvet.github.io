d5ca25d0bcf0524b14f07d4d99e80bb29b113741
Eric Medvet <eric.medvet@gmail.com>
2022-12-24 10:28:01 +0100

    newline

--- update-papers.sh
+++ update-papers.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+
 rm -r content/publications/*
 node update-papers.js 'https://docs.google.com/spreadsheets/d/1V4DHMqSTC0_ACR7T0FvwdPoxX2X92YMdj2fNqrwnuHg/gviz/tq?tqx=out:json' content/publications/ 'Medvet, Eric' 'E'
